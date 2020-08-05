d6b2cb75c7bb664472c4324486966c7140a1e4ab
vinibalbino <silvavinicius2018@gmail.com>
2020-08-05 18:09:48 -0300

    modularizacao e colocacao de variaveis de ambiente.

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 
-const routes = express.Router()
+const routes = express.Router();
 
 const TaskController = require('./controllers/TaskController');
 
 routes.get('/tasks', TaskController.index);
 
-routes.post('/new/task', TaskController.create)
+routes.post('/new/task', TaskController.create);
 
 routes.get('/task/:_id', TaskController.onlyTask);
 
+routes.put('/task/:_id/edit', TaskController.modify);
+
 routes.delete('/task/:_id', TaskController.delete);
 
-module.exports = routes
+module.exports = routes;
